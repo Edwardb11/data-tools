2c27a3302dd1e9ee1ca33883c32be51c94908e31
Edwardb11 <edwardbrito11@hotmail.com>
2023-03-27 20:09:02 -0400

    feat doc and refactor query

--- final-practice/project_final_titanic/models/titanic/passengers_by_class_survival.sql
+++ final-practice/project_final_titanic/models/titanic/passengers_by_class_survival.sql
@@ -1,17 +1,23 @@
 -- Para clasificar el número de pasajeros por clase de viaje que sobrevivieron y no sobrevivieron
 
 {{ config(materialized='table') }}
-
-with raw_data as (
-    select
-        pclass,
-        cast(survived as int) as survived
-    from {{ ref('dbt_supersettrain') }}
-)
-select
+SELECT
     pclass,
-    count(*) as total_passengers,
-    sum(case when survived = 1 then 1 else 0 end) as total_survived,
-    sum(case when survived = 0 then 1 else 0 end) as total_not_survived
-from raw_data
-group by pclass;
+    COUNT(*) AS total_passengers,
+    SUM(CAST(survived AS INTEGER)) AS total_survived,
+    SUM(CASE WHEN survived = '0' THEN 1 ELSE 0 END) AS total_not_survived
+FROM public.dbt_supersettrain
+GROUP BY pclass
+
+/* SELECT: indica que se va a realizar una consulta de datos.
+pclass: es la columna que se va a seleccionar para mostrar en la consulta.
+COUNT(*) AS total_passengers: cuenta el número total de registros y lo renombra como total_passengers.
+SUM(CAST(survived AS INTEGER)) AS total_survived: suma la columna survived que se convierte a tipo INTEGER y lo renombra como total_survived.
+SUM(CASE WHEN survived = '0' THEN 1 ELSE 0 END) AS total_not_survived: suma los registros en los que survived es igual a '0' y lo renombra como total_not_survived.
+FROM public.dbt_supersettrain: especifica la tabla dbt_supersettrain ubicada en el esquema público de la base de datos.
+GROUP BY pclass: agrupa los resultados por valores únicos en la columna pclass.
+*/
+
+
+
+
